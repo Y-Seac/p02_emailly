25844b6bcfa1d2ad92e7aced53319369933bde92
Yanique Seac <seacy@brandeis.edu>
2019-05-02 14:30:55 -0400

    Fixed proxy google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
 
     //gets identifying info and creates a new user in database.
