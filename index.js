913179f7ded23ffc5f72cac102a146fd33425c26
Yanique Seac <seacy@brandeis.edu>
2019-05-16 01:32:03 -0400

    Configured prod build assets

--- index.js
+++ index.js
@@ -27,5 +27,19 @@ app.use(passport.session());
 
 require('./routes/auth')(app); //Basically called the auth.js function and passed the app variable as an param.
 require('./routes/billingRoutes')(app);
+
+//Conditional that signals express that its in production.
+if (process.env.NODE_ENV === 'production') {
+  //Express will give/make available the production assets i.e main.js & main.css
+  app.use(express.static('client/build')); //tells react to look in in client/build directory to see if assets are there.
+
+  //Express will return the index.js file if specific route isnt reconized(not defined in /routes) gets routes.
+  //Catch all case if express cant find what were looking for in any of the directorys above.
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
